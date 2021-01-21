8bdfb5e8f8ece0884f90bc13ba2b9f361153c822
liujunzxcv <273793041@qq.com>
2021-01-21 20:07:41 +0800

    Add files via upload

--- Ipad.py
+++ Ipad.py
@@ -40,36 +40,37 @@ ticks = 1611016660
 def _get_info():
     global origin_position
     while True:
-        try:
-            url = 'https://www.apple.com.cn/shop/refurbished/ipad'
-
-            headers = {
-                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
-            }
-            r = requests.get(url, headers=headers)
-            r.encoding = r.apparent_encoding
-            html = r.text
-            soup = BeautifulSoup(html, 'lxml')  # 转换为BeautifulSoup的解析对象()里第二个参数为解析方式
-            # titles = soup.find_all('a', class_='as-producttile-tilelink')
-            titles = soup.find_all(text=re.compile('(?=.*翻新 iPad )'))
-            result = ''
-            for each in titles:
-                if each.find("GB") != -1 & each.find("\n") == -1:
-                    result += each + '\n'
-            if result.__len__() > 0:
-                time2 = time.time()
-                global ticks 
-                timediff = time2 - ticks
-                logger.info("距离上次发邮件小时数" + str(timediff/60/60))
-                if timediff > 86400:
+        now = time.time()
+        global ticks 
+        timediff = now - ticks
+        logger.info("距离上次发邮件小时数" + str(timediff/60/60))
+        if timediff > 86400:
+            try:
+                url = 'https://www.apple.com.cn/shop/refurbished/ipad'
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
+                }
+                r = requests.get(url, headers=headers)
+                r.encoding = r.apparent_encoding
+                html = r.text
+                soup = BeautifulSoup(html, 'lxml')  # 转换为BeautifulSoup的解析对象()里第二个参数为解析方式
+                # titles = soup.find_all('a', class_='as-producttile-tilelink')
+                titles = soup.find_all(text=re.compile('(?=.*翻新 iPad Pro)'))
+                result = ''
+                for each in titles:
+                    if each.find("GB") != -1 & each.find("\n") == -1:
+                        result += each + '\n'
+                if result.__len__() > 0:
                     send_email = Mail(text=result, sender='wo', receiver='ni', subject='iPad Pro 有货', logger=logger,
                                   smtp_server=smtp_server, from_addr=from_addr, password=password)
                     send_email.send()
-                    ticks = time2
-            else:
-                logger.info("商品无货" + result)
-        except Exception as e:
-            logger.error(e)
+                    ticks = now
+                else:
+                    logger.info("商品无货" + result)
+            except Exception as e:
+                logger.error(e)
+        else:
+            logger.info("无需查询")
         time.sleep(600)
 
 
